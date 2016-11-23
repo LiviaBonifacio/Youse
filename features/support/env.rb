006c0f3f87b754bbfe233ad5fab46b3e611c9db3
Lívia Bonifácio <liviabonifacio@HDU15526-2.local>
2016-11-23 16:22:54 -0200

    ..

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,14 +2,12 @@ require "capybara/cucumber"
 require "selenium-webdriver"
 require "site_prism"
 require "faker"
-require 'capybara-screenshot/cucumber'
-require 'capybara-screenshot/rspec'
 
-#Capybara.register_driver :chrome do |app|
-#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
-#end
+Capybara.register_driver :chrome do |app|
+   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
 
-#Capybara.default_driver = :chrome
-Capybara.default_driver = :selenium
+Capybara.default_driver = :chrome
+#Capybara.default_driver = :selenium
 Capybara.default_max_wait_time = 20
 Capybara.javascript_driver = :webkit
